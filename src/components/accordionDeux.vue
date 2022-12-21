7f4639f159be36eebf17e136f32c19fd55b7000a
Emestay <103575914+Emestay@users.noreply.github.com>
2022-12-21 10:03:20 +0100

    border

--- src/components/accordionDeux.vue
+++ src/components/accordionDeux.vue
@@ -1,90 +1,101 @@
 <template>
-    <div>
-      <template v-for="(item, index) in items" :key="index">
-        <div class="accordion-item" >
-          <div @click="toggleAccordion(index)" class="accordion-header " :class="{ 'active': item.open }">
-            {{ item.header }}
-          </div>
-          <div v-if="item.open" class="accordion-body ">
-            {{ item.body }}
-          </div>
+  <div>
+    <template v-for="(item, index) in items" :key="index">
+      <div class="accordion-item">
+        <div @click="toggleAccordion(index)" class="accordion-header " :class="{ 'active': item.open }">
+          {{ item.header }}
         </div>
-      </template>
-    </div>
-  </template>
+        <div v-if="item.open" class="accordion-body ">
+          {{ item.body }}
+        </div>
+        <div v-else="!item.open" class="hiddenfrr" >
+          
+        </div>
+        <br>
+      </div>
+    </template>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'AccordionDeux',
-    data() {
-      return {
-        items: [
-          {
-            header: 'Header 1',
-            body: 'Body 1 lorem Body 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 lorem',
-            open: null
-          },
-          {
-            header: 'Header 2',
-            body: 'Body 1 lorem Body 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 lorem',
-            open: null
-          },
-          {
-            header: 'Header 3',
-            body: 'Body 1 lorem Body 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 lorem',
-            open: null
-          }
-        ]
-      }
-    },
-    methods: {
-      toggleAccordion(index) {
-        this.items.forEach((item, i) => {
-          if (index === i) {
-            item.open = !item.open
-          } else {
-            item.open = false
-          }
-        })
-      }
-    },
-    scroll(e){
+<script>
+export default {
+  name: 'AccordionDeux',
+  data() {
+    return {
+      items: [
+        {
+          header: 'Header 1',
+          body: 'Body 1 lorem Body 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 lorem',
+          open: null
+        },
+        {
+          header: 'Header 2',
+          body: 'Body 1 lorem Body 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 lorem',
+          open: null
+        },
+        {
+          header: 'Header 3',
+          body: 'Body 1 lorem Body 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 loremBody 1 lorem',
+          open: null
+        }
+      ]
+    }
+  },
+  methods: {
+    toggleAccordion(index) {
+      this.items.forEach((item, i) => {
+        if (index === i) {
+          item.open = !item.open
+        } else {
+          item.open = false
+        }
+      })
+    }
+  },
+  scroll(e) {
 
-    },
-    
-  }
-  </script>
+  },
+
+}
+</script>
   
-  <style>
-  *{
-    color: aqua;
-  }
-  .accordion-item {
-    border: 1px solid #ccc;
-  }
+<style>
+* {
+  color: aqua;
+}
+
+.accordion-item {
+  border: 2px black solid;
+  box-shadow: 10px 10px 0px 0px rgba(0, 0, 0, 1);
+  -webkit-box-shadow: 10px 10px 0px 0px rgba(0, 0, 0, 1);
+  -moz-box-shadow: 10px 10px 0px 0px rgba(0, 0, 0, 1);
   
-  .accordion-header {
-    background-color: #f5f5f5;
-    cursor: pointer;
-    padding: 18px;
-    width: 100%;
-    border: none;
-    text-align: left;
-    outline: none;
-    font-size: 15px;
-    transition: 0.4s;
-  }
-  .active{
-    color: chartreuse;
-  }
-  .active, .accordion-header:hover {
-    background-color: brown;
-  }
-  
-  .accordion-body {
-    padding: 0 18px;
-    
-    overflow: hidden;
-    background-color: white;
-  }
-  </style>
+}
+
+.accordion-header {
+  background-color: #f5f5f5;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  text-align: left;
+  font-size: 15px;
+  transition: 0.4s;
+}
+
+.active {
+  color: chartreuse;
+}
+
+.active,
+.accordion-header:hover {
+  background-color: brown;
+}
+
+.accordion-body {
+  background-color: white;
+}
+
+.hiddenfrr{
+  display: none;
+}
+</style>
